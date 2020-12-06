b48c4fec17e6504b4759d326cdce093111a616c2
yhjaichirou <835057159@qq.com>
2020-12-06 17:14:40 +0800

    sss

--- branchBigData/js/yhj.js
+++ branchBigData/js/yhj.js
@@ -1,8 +1,9 @@
-
-
 	var htmlPage = ['index.html','member.html','',''];
 	$(".rightTabBtn>div").click(function(){
 		var indee = $(this).index();
 		console.log(indee);
-		$("#root").find(".ant-spin-container").();
+		$.ajax({ type:"get", url:"../member.html",async:false, success : function(result){ 
+			alert(result); 
+		} );
+		$("#root").find(".ant-spin-container").html($());
 	})
